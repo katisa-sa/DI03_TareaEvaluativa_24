bf5a6938d3669eed46b4c7d0b8a4456798e5cfa9
Katerin Sanz Celorrio <ksanz@birt.eus>
2024-11-26 10:44:57 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DI03_TareaEvaluativa_Noticias_24',
+  webDir: 'www'
+};
+
+export default config;
